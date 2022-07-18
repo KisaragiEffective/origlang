3d2668f3fb49130319c64eb92f89b331dea35745
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-07-19 01:37:53 +0900

    test: テストで期待される値も記述するようにする

--- src/main.rs
+++ src/main.rs
@@ -11,73 +11,50 @@ use ast::{First, RootAst, Statement};
 use runtime::Runtime;
 use crate::lexer::Lexer;
 
+#[allow(clippy::unreadable_literal)]
 fn main() -> Result<(), String> {
     eprintln!("start");
-    integrated_test("123456\n")?;
-    integrated_test("1\n2\n")?;
-    integrated_test("var x = 1\nx\n")?;
-    integrated_test("var x = 1\nvar y = x\ny\n")?;
+    assert_eq!(expression_list("123456\n")?, vec![123456]);
+    assert_eq!(expression_list("1\n2\n")?, vec![1, 2]);
+    assert_eq!(expression_list("var x = 1\nx\n")?, vec![1]);
+    assert_eq!(expression_list("var x = 1\nvar y = x\ny\n")?, vec![1]);
     // plus operator test (binary)
-    integrated_test("1 + 2\n")?;
-    integrated_test("var x = 1\n1 + x\n")?;
-    integrated_test("var x = 1\nvar y = 3\nx + y\n")?;
+    assert_eq!(expression_list("1 + 2\n")?, vec![3]);
+    assert_eq!(expression_list("var x = 1\n1 + x\n")?, vec![2]);
+    assert_eq!(expression_list("var x = 1\nvar y = 3\nx + y\n")?, vec![4]);
 
     // plus operator test (more than twice)
-    integrated_test("var x = 1\nvar y = 2\nvar z = 3\nx + y + z\n")?;
+    assert_eq!(expression_list("var x = 1\nvar y = 2\nvar z = 3\nx + y + z\n")?, vec![6]);
 
     // minus operator test (binary)
-    integrated_test("1 - 2\n")?;
-    integrated_test("var x = 1\n1 - x\n")?;
-    integrated_test("var x = 1\nvar y = 3\nx - y\n")?;
+    assert_eq!(expression_list("1 - 2\n")?, vec![-1]);
+    assert_eq!(expression_list("var x = 1\n1 - x\n")?, vec![0]);
+    assert_eq!(expression_list("var x = 1\nvar y = 3\nx - y\n")?, vec![-2]);
 
     // minus operator test (more than twice)
-    // NOTE: should be zero
-    integrated_test("var x = 1\nvar y = 2\nvar z = 3\nz - x - y\n")?;
+    assert_eq!(expression_list("var x = 1\nvar y = 2\nvar z = 3\nz - x - y\n")?, vec![0]);
 
     // paren test
-    // should be 1
-    integrated_test("(1)\n")?;
-    // should be 2
-    integrated_test("3 - (2 - 1)\n")?;
-    // should be 0
-    integrated_test("(3 - 2) - 1\n")?;
+    assert_eq!(expression_list("(1)\n")?, vec![1]);
+    assert_eq!(expression_list("3 - (2 - 1)\n")?, vec![2]);
+    assert_eq!(expression_list("(3 - 2) - 1\n")?, vec![0]);
 
     // multiply test
-    // should be 6
-    integrated_test("3 * 2\n")?;
-    // should be 7, should not be 9
-    integrated_test("3 * 2 + 1\n")?;
-    // should be 9
-    integrated_test("3 * (2 + 1)\n")?;
+    assert_eq!(expression_list("3 * 2\n")?, vec![6]);
+    assert_eq!(expression_list("3 * 2 + 1\n")?, vec![7]);
+    assert_ne!(expression_list("3 * 2 + 1\n")?, vec![9]);
+    assert_eq!(expression_list("3 * (2 + 1)\n")?, vec![9]);
+    assert_ne!(expression_list("3 * (2 + 1)\n")?, vec![7]);
 
     eprintln!("end");
     Ok(())
 }
 
-fn integrated_test(src: &str) -> Result<(), String> {
-    test_source_with_lexer(src);
-    test_source(src)
-}
-
-fn test_source(src: &str) -> Result<(), String> {
+fn expression_list(src: &str) -> Result<Vec<i32>, String> {
     use crate::parser::Parser;
-    eprintln!("source_test: ----------");
-    eprint!("{src}");
-    eprintln!("------------------");
     let source = src;
     let parser = Parser::create(source);
     let root_ast = parser.parse()?;
     let runtime = Runtime::create(root_ast);
-    runtime.execute();
-    Ok(())
-}
-
-fn test_source_with_lexer(src: &str) {
-    eprintln!("lexer: ----------");
-    eprint!("{src}");
-    eprintln!("------------------");
-    let lexer = Lexer::create(src);
-    (0..9).into_iter().map(|_| lexer.next()).for_each(|token| {
-        println!("{token:?}");
-    });
+    Ok(runtime.yield_all_evaluated_expressions())
 }
