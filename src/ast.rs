918f57d88c114a1fe2dd3976325ffbb8b898b3ad
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-07-19 20:31:10 +0900

    feat: add derived Debug

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,11 @@
 /// 現時点のプログラムとは、プリントするべき式の列である
 #[allow(clippy::module_name_repetitions)]
+#[derive(Debug)]
 pub struct RootAst {
     pub(crate) statement: Vec<Statement>
 }
 
+#[derive(Debug)]
 pub enum Statement {
     /// <int_literal> <new_line>
     Print {
