18054d544685dce13e8a5bb930f7e21cb6bc5c30
Kisaragi Marine <kisaragi.effective@gmail.com>
2022-12-27 03:10:45 +0900

    feat: print expression's type in REPL

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 use crate::parser::SimpleErrorWithPos;
+use crate::runtime::RuntimeError;
 use crate::type_check::error::TypeCheckError;
 
 #[derive(Error, Debug)]
@@ -8,4 +9,6 @@ pub enum AllError {
     Generic(#[from] SimpleErrorWithPos),
     #[error("{0}")]
     TypeCheck(#[from] TypeCheckError),
+    #[error("{0}")]
+    Runtime(#[from] RuntimeError),
 }
