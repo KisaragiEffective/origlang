9cd7096eaaa7e91b75c23a760b1b18de4a6de4f8
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-04-04 06:43:04 +0900

    feat(frontend): load example from file system

--- package/origlang-interop-frontend-webserver/build.rs
+++ package/origlang-interop-frontend-webserver/build.rs
@@ -6,6 +6,8 @@ fn main() {
     let cd = std::env::current_dir().expect("<fail>");
     println!("cd: {}", cd.display());
 
+    let www = cd.join("www");
+
     let interop_dir = cd.parent().expect("no parent").join("origlang-interop").join("pkg");
     println!("interop_dir: {}", interop_dir.display());
 
@@ -19,13 +21,33 @@ fn main() {
         let common_parent = from.iter().take(from.components().count() - 3).collect::<PathBuf>();
         println!("cpar: {}", common_parent.display());
 
-        let to = common_parent
-            .join("origlang-interop-frontend-webserver")
-            .join("www")
-            .join(name);
+        let to = www.join(name);
 
         println!("copy: {from}", from = &from.display());
         println!("copy: {to}", to = &to.display());
         copy_file(from, to).expect("copy failed, please build origlang-interop first.");
     }
+
+    let useful_example_dir =         cd.parent().expect("parent")
+        .parent().expect("parent.parent")
+        .join("compile")
+        .join("positive")
+        .join("non-perf");
+
+    let www_example_dir = www.join("example");
+    if !www_example_dir.exists() {
+        std::fs::create_dir_all(&www_example_dir).expect("www/example");
+    }
+    std::fs::copy(
+        useful_example_dir.join("99_bottles_of_beer.ol"),
+        www_example_dir.join("99_bottles_of_beer.ol")
+    ).expect("99 bottles");
+    std::fs::copy(
+        useful_example_dir.join("hello_world.ol"),
+        www_example_dir.join("hello_world.ol")
+    ).expect("Hello world");
+    std::fs::copy(
+        useful_example_dir.join("fizz_buzz.ol"),
+        www_example_dir.join("fizz_buzz.ol")
+    ).expect("Fizz Buzz");
 }
