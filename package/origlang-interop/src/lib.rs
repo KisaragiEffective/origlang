a56f4a85c5af87856278140f357d1e8960147d4c
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-04-03 23:36:50 +0900

    chore(perf): inserted perf monitor code

--- package/origlang-interop/src/lib.rs
+++ package/origlang-interop/src/lib.rs
@@ -1,6 +1,7 @@
 use js_sys::JsString;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen::prelude::wasm_bindgen;
+use web_sys::console;
 use origlang_compiler::parser::Parser;
 use origlang_runtime::{OutputAccumulator, Runtime, TypeBox};
 
@@ -43,18 +44,53 @@ impl OutputAccumulator for PseudoStdout {
     }
 }
 
+struct Timer<'a> {
+    name: &'a str
+}
+
+impl<'a> Timer<'a> {
+    fn new(name: &'a str) -> Self {
+        console::time_with_label(name);
+        Self { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
+
 #[wasm_bindgen]
 pub fn run() {
-    init_panic_hook();
-
-    let src = get_source();
-    if let Ok(src) = src.dyn_ref::<JsString>() {
+    let _ = Timer::new("entire");
+    let src = {
+        let _ = Timer::new("init");
+        init_panic_hook();
+        get_source()
+    };
+    if let Some(src) = src.dyn_ref::<JsString>() {
         let src = src.as_string().expect("Source code must not contain invalid surrogate codepoint");
-        let parser = Parser::create(&src);
-        match parser.parse() {
+        let parser = {
+            let _ = Timer::new("parse.construction");
+            Parser::create(&src)
+        };
+        let res = {
+            let _ = Timer::new("parse");
+            parser.parse()
+        };
+        match res {
             Ok(ast) => {
-                let runtime = Runtime::create(PseudoStdout);
-                let instructions = runtime.what_will_happen(ast);
+                let _ = Timer::new("runtime");
+                let runtime = {
+                    let _ = Timer::new("runtime.construction");
+                    Runtime::create(PseudoStdout)
+                };
+                let instructions = {
+                    let _ = Timer::new("runtime.intermediate");
+                    runtime.what_will_happen(ast)
+                };
+                let _ = Timer::new("runtime.execution");
                 for instruction in instructions {
                     instruction.invoke(&runtime);
                 }
