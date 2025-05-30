8e4441e7292591b6b17fa53b238987f20cccd608
Kisaragi <48310258+KisaragiEffective@users.noreply.github.com>
2025-05-30 11:25:52 +0900

    chore(cli): ignore clippy::ressult_large_err

--- package/origlang-cli/src/main.rs
+++ package/origlang-cli/src/main.rs
@@ -9,6 +9,7 @@ mod args;
 mod error;
 mod task;
 
+#[expect(clippy::result_large_err)] // #739 で急に出てきた
 fn main() -> Result<(), TaskExecutionError> {
     let args = Args::parse();
     env_logger::init();
